12d7fdfd5989cdc31987aee57e6e3d640a1159d4
lohitha-anumala <68274764+lohitha-anumala@users.noreply.github.com>
2020-08-05 23:30:13 +0530

    Add files via upload

---
+++ LinkedListAssignment/ListAssClass1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListAssignment
+{
+
+    class Person
+    {
+        int age;
+        string fname;
+        string lname;
+        string city;
+        public Person()
+        {
+
+        }
+        public Person(int _age,string _fname,string _lname,string _city)
+        {
+            age = _age;
+            fname = _fname;
+            lname = _lname;
+            city = _city;
+
+        }
+        public void DisplayPerson()
+        {
+            Console.WriteLine("Age :{0}", age);
+            Console.WriteLine("FirstName:{0}", fname);
+            Console.WriteLine("LastName:{0}", lname);
+            Console.WriteLine("City:{0}", city);
+        }
+
+
+    }
+    class ListAssClass1
+    {
+        public static void Main()
+        {
+           // List<Person>  = new List<Person>();
+            List<Person> P = new List<Person>();
+            Person Per = new Person(21,"Lohitha","Anumala","Vjayawada");
+
+
+            Person per2 = new Person(21, "Asmin", "Shaik", "Tadepalli");
+            Per.DisplayPerson();
+            P.Add(per2);
+            per2.DisplayPerson();
+
+
+
+            
+
+           
+        }
+    }
+}
---
+++ LinkedListAssignment/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace LinkedListAssignment
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            LinkedList<string> IList = new LinkedList<string>(new string[] { "Quick", "Fox", "the", "Lazy" });
+            foreach (string str in IList)
+            {
+                Console.Write(str + " ");
+            }
+            LinkedListNode<string> current = IList.Find("Fox");
+            Console.WriteLine();
+            Console.WriteLine("---- ----------------------------Find------------------------------------");
+            foreach (string str in IList)
+            {
+                Console.Write(str + " ");
+            }
+            IList.AddFirst("the");
+            Console.WriteLine();
+
+            Console.WriteLine("----------------------------AddFirst()-----------------------------------------------");
+          //  Console.WriteLine("After using Find");
+            foreach (string str in IList)
+            {
+                Console.Write(str + " ");
+            }
+
+            IList.AddBefore(current, "brown");
+            Console.WriteLine();
+
+            Console.WriteLine("---------------------------------AddBefore()-----------------------------");
+            foreach (string str in IList)
+            {
+                Console.Write(str + " ");
+            }
+            IList.AddAfter(current, "Jumps over");
+            Console.WriteLine();
+            Console.WriteLine("------------------------ AddAfter()---------------------------");
+            foreach (string str in IList)
+            {
+                Console.Write(str + " ");
+            }
+            IList.AddLast("fox");
+            Console.WriteLine();
+
+            Console.WriteLine("--------------------AddLast-------------------------");
+           
+            foreach (string str in IList)
+            {
+                Console.Write(str + " ");
+            }
+
+           
+            IList.FindLast( "fox");
+
+
+
+
+
+        }
+    }
+}
